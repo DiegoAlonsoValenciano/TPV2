d6a09e47235c8f8b5f25e9367a43198b62494ee6
Jiale <jialehe28@gmail.com>
2024-02-18 18:58:03 +0100

    Terminado el opposite

--- TPV2-Ej1/TPV2/src/game/ShowAtOpposieSide.cpp
+++ TPV2-Ej1/TPV2/src/game/ShowAtOpposieSide.cpp
@@ -13,4 +13,13 @@ void ShowAtOpposieSide::update(Container* o) {
 	if (pos.getX()< -o->getWidth()) {
 		pos = Vector2D(sdlutils().width()-5, pos.getY());
 	}
+	if (pos.getX() > sdlutils().width()) {
+		pos = Vector2D(-o->getWidth(), pos.getY());
+	}
+	if (pos.getY() < -o->getHeight()) {
+		pos = Vector2D(pos.getX(), sdlutils().height());
+	}
+	if (pos.getY() > sdlutils().height()) {
+		pos = Vector2D(pos.getX(), -o->getHeight());
+	}
 }
