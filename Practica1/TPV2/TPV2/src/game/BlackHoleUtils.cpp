40490d940868e1a5100f559543bf834e020538a2
Jiale <jialehe28@gmail.com>
2024-02-26 17:51:55 +0100

    agujro negro

--- Practica1/TPV2/TPV2/src/game/BlackHoleUtils.cpp
+++ Practica1/TPV2/TPV2/src/game/BlackHoleUtils.cpp
@@ -1,7 +1,19 @@
 #include "BlackHoleUtils.h"
+#include "../components/Image.h"
+#include "../components/Transform.h"
+#include "../ecs/Manager.h"
+#include "../sdlutils/SDLUtils.h"
+#include "Game.h"
 
+BlackHoleUtils::BlackHoleUtils():rand_(sdlutils.rand()),width_(sdlutils.width()),height_(sdlutils.height())
+{
+	rot_ = rand_.nextInt(5, 10);
+};
 void BlackHoleUtils::create_BlackHole()
 {
+	for (int i = 0; i < 6; i++) {
+		int y = 0, x = 0;
+	}
 }
 
 void BlackHoleUtils::remove_BlackHole()
--- Practica1/TPV2/TPV2/src/game/BlackHoleUtils.h
+++ Practica1/TPV2/TPV2/src/game/BlackHoleUtils.h
@@ -7,10 +7,15 @@ class RandomNumberGenerator;
 class BlackHoleUtils : public BlackHoleFacade
 {
 public: 
-	BlackHoleUtils() {};
+	BlackHoleUtils();
 	virtual ~BlackHoleUtils() {};
 	void create_BlackHole() override;
 	void remove_BlackHole() override;
 
 private:
+	void generateBlackHole(const Vector2D& p, const Vector2D& v, int g);
+	RandomNumberGenerator& rand_;
+	int width_;
+	int height_;
+	int rot_;
 };
