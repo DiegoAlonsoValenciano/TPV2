bbb95e54fe0c67bc580d926c5e0a43c9b0fde1ec
DiegoAlonsoValenciano <125390412+DiegoAlonsoValenciano@users.noreply.github.com>
2024-02-26 17:37:53 +0100

    missile

---
+++ Practica1/TPV2/TPV2/src/game/MissileFacade.h
@@ -0,0 +1,15 @@
+// This file is part of the course TPV2@UCM - Samir Genaim
+
+#pragma once
+#include "../ecs/ecs.h"
+
+using ecs::entity_t;
+
+class MissileFacade
+{
+public:
+	MissileFacade(){}
+	virtual ~MissileFacade(){}
+	virtual void generate_missile() = 0;
+	virtual void remove_all_missile() = 0;
+};
